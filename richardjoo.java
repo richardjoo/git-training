18ebc2276c1b70c23288619c8f3919608a70d7c0
Richard Joo <richardjoo@gmail.com>
2011-02-25 17:04:56 -0800

    Working on HelloWorld class

--- richardjoo.java
+++ richardjoo.java
@@ -1,4 +1,8 @@
+import java.util.*;
+
 public class HelloWorld {
+	String[] names = {"Spanky", "Alfalfa", "Buckwheat", "Daria", "Stymie", "Marianne", "Scotty", "Tommy", "Chubby"};
+	
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
 	}

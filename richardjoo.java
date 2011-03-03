0bccb175ea68f967d16ef643bf76300b2a4ada49
Richard Joo <richardjoo@gmail.com>
2011-03-03 11:26:12 -0800

    modified richardjoo.java file

--- richardjoo.java
+++ richardjoo.java
@@ -1,11 +1,2 @@
-<<<<<<< HEAD
-Hello World!!!!
-Line added
-
-=======
-public calss HelloWorld {
-		public static void main(String[] args) {
-				System.out.println("Hello World!");
-			}
-}
+// hahahahah get this!
 
